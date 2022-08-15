670c40e2ab2f8c0f48e61ad5d98a47dc7deecb6f
DimaM <iPasaremos45@yandex.ru>
2022-08-15 09:22:26 +0300

    Добавлен роекет

---
+++ js/friends.js
@@ -0,0 +1,15 @@
+
+function listFriends() {
+
+  if (true) {
+    document.getElementById('list-friends').style.display="block";
+    document.getElementById('add-friends').style.display="none";
+  }
+}
+
+function addFriends() {
+  if (true) {
+    document.getElementById('add-friends').style.display="block";
+    document.getElementById('list-friends').style.display="none";
+  }
+}
